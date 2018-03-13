2efe4668a58aa591883498c9bfc7b70bf986960f
Quangang Sheng <shengquangang2015@163.com>
2018-03-13 19:06:26 +0800

    add openssl for SHA256 and HMAC

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -6,7 +6,12 @@
 
 #include "../src/server/supl-server.hpp"
 
+#include <openssl/sha.h>
+
 int main(int argc, char* argv[]) try {
+    ::SHA256_CTX ctx;
+    ::SHA256_Init(&ctx);
+
     return EXIT_SUCCESS;
 } catch (std::exception const &e) {
     std::cerr << "[C++ exception] " << e.what() << std::endl;
