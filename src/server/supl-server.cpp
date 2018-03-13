da09262713793d20d8b33fbeb5b7712e54506a82
Quangang Sheng <shengquangang2015@163.com>
2018-03-13 18:02:32 +0800

    add asn1 files

---
+++ src/server/supl-server.cpp
@@ -0,0 +1,11 @@
+#include "supl-server.hpp"
+
+SUPL_SERVER_BEGIN_DECLS
+
+server::server() {
+}
+
+server::~server() {
+}
+
+SUPL_SERVER_END_DECLS
---
+++ src/server/supl-server.hpp
@@ -0,0 +1,16 @@
+#ifndef SUPL_SERVER_SERVER_SUPL_SERVER_HPP_INCLUDED
+#define SUPL_SERVER_SERVER_SUPL_SERVER_HPP_INCLUDED
+
+#include "macros.hpp"
+
+SUPL_SERVER_BEGIN_DECLS
+
+class server {
+    public:
+        server();
+        virtual ~server();
+};
+
+SUPL_SERVER_END_DECLS
+
+#endif // SUPL_SERVER_SERVER_SUPL_SERVER_HPP_INCLUDED
