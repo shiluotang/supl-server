611ab8bb11d5c079b9a8e2bec09e2c3bf60fff90
Quangang Sheng <shengquangang2015@163.com>
2018-03-14 11:39:22 +0800

    maybe not working.

--- src/asn1c/codec.cpp
+++ src/asn1c/codec.cpp
@@ -1,7 +1,68 @@
+#include <ostream>
+#include <sstream>
+#include <stdexcept>
+
 #include "codec.hpp"
+
+extern "C" {
+
+    static int xer_encode_stream_callback(const void *buffer, size_t size,
+            void *application_specific_key) {
+        if (!application_specific_key)
+            return -1;
+        std::ostream &os = *reinterpret_cast<std::ostream*>(application_specific_key);
+        os.write(reinterpret_cast<char const*>(buffer), size);
+        return !!os ? 0 : -1;
+    }
+
+    static int xer_encode_buffer_callback(const void *buffer, size_t size,
+            void *application_specific_key) {
+        typedef org::sqg::supl::xer_codec::buffer_type buffer_type;
+        typedef org::sqg::supl::xer_codec::byte byte;
+        if (!application_specific_key)
+            return -1;
+        buffer_type &storage = *reinterpret_cast<buffer_type*>(application_specific_key);
+        byte const* p = reinterpret_cast<byte const*>(buffer);
+        storage.insert(storage.end(), p, p + n);
+        return 0;
+    }
+}
 
 SUPL_SERVER_BEGIN_DECLS
 
 
+bool xer_codec::encode0(::asn_TYPE_descriptor_t &td,
+        void *struct_ptr,
+        std::ostream &os,
+        bool throwable) {
+    // TODO Not implemented yet!
+    std::stringstream ss;
+    asn_enc_rval_t rc = ::xer_encode(&td, struct_ptr, XER_F_BASIC, &xer_encode_stream_callback, &ss);
+    if (rc.encoded == -1) {
+        if (throwable) {
+            std::stringstream es;
+            es << "xer encode failed at " << rc.failed_type->name;
+            throw std::runtime_error(es.str());
+        }
+        return false;
+    }
+    return true;
+}
+
+bool xer_codec::encode0(::asn_TYPE_descriptor_t &td,
+        void *struct_ptr,
+        xer_codec::buffer_type &buffer,
+        bool throwable) {
+    asn_enc_rval_t rc = ::xer_encode(&td, struct_ptr, XER_F_BASIC, &xer_encode_buffer_callback, &ss);
+    if (rc.encoded == -1) {
+        if (throwable) {
+            std::stringstream es;
+            es << "xer encode failed at " << rc.failed_type->name;
+            throw std::runtime_error(es.str());
+        }
+        return false;
+    }
+    return true;
+}
 
 SUPL_SERVER_END_DECLS
--- src/asn1c/codec.hpp
+++ src/asn1c/codec.hpp
@@ -21,24 +21,72 @@ private:
 
 class xer_codec {
 public:
-    typedef unsigned char ubyte;
-    typedef std::vector<ubyte> buffer_t;
+    typedef unsigned char byte;
+    typedef std::vector<byte> buffer_type;
 public:
     template <typename T>
-    static bool encode(::asn_TYPE_descriptor_t &td, T &instance, buffer_t &bytes) {
-        return true;
+    static bool encode(::asn_TYPE_descriptor_t &td, T &instance, buffer_type &buffer) {
+        return encode0(td, &instance, buffer, false);
     }
 
     template <typename T>
-    static buffer_t encode(::asn_TYPE_descriptor_t &td, T &instance) {
-        buffer_t buffer;
+    static bool encode(::asn_TYPE_descriptor_t &td, T &instance, std::ostream &os) {
+        return encode0(td, &instance, os, false);
+    }
+
+    template <typename T>
+    static buffer_type encode(::asn_TYPE_descriptor_t &td, T &instance) {
+        buffer_type buffer;
         if (!encode(instance, buffer))
             throw std::runtime_error("xer encode failed!");
         return buffer;
     }
+
+    template <typename T>
+    static std::string encode(::asn_TYPE_descriptor_t &td, T &instance) {
+        std::stringstream ss;
+        if (encode0(td, &instance, ss, true)) try {
+            return ss.str();
+        } catch (...) {
+            throw;
+        }
+    }
 private:
-    static ::asn_enc_rval_t encode0(::asn_TYPE_descriptor_t&, void*, std::ostream&);
-    static ::asn_enc_rval_t encode0(::asn_TYPE_descriptor_t&, void*, buffer_t&);
+    /**
+     * Encode ASN.1 struct into std::ostream.
+     *
+     * @param [in] td The ASN.1 element type descriptor.
+     * @param [in] struct_ptr The ASN.1 element C struct pointer.
+     * @param [in] throwable Whether to throw exception on error.
+     * @return true on success, vice versa.
+     */
+    static bool encode0(::asn_TYPE_descriptor_t &td, void *struct_ptr, std::ostream &os, bool throwable);
+    static bool encode0(::asn_TYPE_descriptor_t&, void*, buffer_type&, bool);
+
+    static void* decode0(::asn_TYPE_descriptor_t&, void const*, size_t, bool);
+    static void* decode0(::asn_TYPE_descriptor_t&, std::string const&, bool);
+    static void* decode0(::asn_TYPE_descriptor_t&, buffer_type const&, bool);
+};
+
+class uper_codec {
+public:
+    typedef unsigned char byte;
+    typedef std::vector<byte> buffer_type;
+public:
+    template <typename T>
+    bool encode(::asn_TYPE_descriptor_t &td, T &instance, buffer_type &buffer) {
+        // TODO not implemented yet!
+        return true;
+    }
+    
+    template <typename T>
+    buffer_type encode(::asn_TYPE_descriptor_t &td, T &instance) {
+        // TODO not implemented yet!
+        return buffer_type();
+    }
+
+private:
+    static bool encode0(::asn_TYPE_descriptor_t&, void*, buffer_type&, bool);
 };
 
 SUPL_SERVER_END_DECLS
